4cbbb02f421045a71f064f3bb75018488c1a12d6
Shijo <shijo9345@gmail.com>
2024-07-02 01:13:27 +0530

    Added operator.js

---
+++ operator.js
@@ -0,0 +1,33 @@
+/*Operators
+-----------
+
+Arithmetic operators  +  -  *  **  /  %  ++  --
+Assignment Operators  =  +=  -=  *=  /=  %=  **= 
+Comparison Operators  == === != !=== > < >= <= 
+Logical Operators     &&  ||  !
+Bitwise Operatora     &  |  ~  <<  >>  >>>
+
+*/
+a=10
+b=20
+console.log(a*b+a-b)     // without paranthesis
+console.log(a*(b+a)-b)   // with paranthesis
+console.log(a**2)        // Square of a
+console.log(a**3)        // Cube of a
+console.log(a%b)         // remainder
+console.log(a/b)         // Quotient
+console.log(a++)         // Post increament
+console.log(++a)         // pre increament
+
+
+a=10
+b=20
+console.log(a**=2)    //Assignment operator
+
+console.log(a/0)              // Infinity
+console.log(0/0)              // NaN
+console.log(Math.sqrt(-1))    // NaN
+
+
+
+
