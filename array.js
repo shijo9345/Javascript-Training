12ffd7559413d4347254524517ce6361446d1045
Shijo <130920346+shijo9345@users.noreply.github.com>
2024-07-04 09:05:01 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,23 @@
+// Array - To hold more than one value
+const cars = [
+    "Saab",
+    "Volvo",
+    "BMW"
+  ]
+  console.log(cars)
+
+  //You can also create an array,
+  // and then provide the elements
+
+  const car=[]
+  car[0]=1
+  car[1]=2
+  car[2]=3
+  car[3]=4
+  console.log(car)
+
+  const bike= new Array(2,3,4,5)  //another method using keyword new
+  console.log(bike)
+
+  const myArr = [[1,2],[3,4],[5,6]];
+  console.log(myArr.flat());
